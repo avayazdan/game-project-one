3d9bdf35921e22f43eec16001a3650c3d88e9379
Ava Yazdany <avayaz1337@gmail.com>
2022-01-18 21:47:53 +0100

    movement function added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const myGrid = document.querySelector('.grid')
 const width = 15
 const height = 15
 
+const audioShoot = new Audio('audio/shoot.mp3')
+
 let score = 0
 const scoreBoard = document.querySelector('#score')
 
@@ -14,26 +16,104 @@ for (let i = 0; i < width * height; i++) {
   myGrid.appendChild(square)
 }
 
-const aliens = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26,28,30,32,34,36,38,40]
+
 const squares = Array.from(document.querySelectorAll('.grid div'))
 console.log(squares)
 
 // adding elements onto page
+
+let interval
+let intervalSpeed = 2000
+const aliens = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
 
 function drawAliens() {
   for (let i = 0; i < aliens.length; i++) {
     squares[aliens[i]].classList.add('alien')
   }
 }
-
 drawAliens()
+
+const aliensIndex = Array.from(aliens)
+
+function moveAliens(indexChange) {
+  const rightWall = (aliensIndex) => (aliensIndex + 1) % 14 === 0
+  const leftWall = (aliensIndex) => aliensIndex === 0 || aliensIndex % 14 === 0
+  switch (indexChange) {
+    case 1:
+      if (aliensIndex.some(rightWall)) {
+        stopAliens(indexChange)
+        return
+      }
+      break
+    case -1:
+      if (aliensIndex.some(leftWall)) {
+        stopAliens(indexChange)
+        return
+      }
+      break
+  }
+  for (let i = 0; i < aliensIndex.length; i++) {
+    squares[aliensIndex[i]].classList.remove('alien')
+    aliensIndex[i] = aliensIndex[i] + indexChange
+    squares[aliensIndex[i]].classList.add('alien')
+  }
+}
+
+function stopAliens(indexChange) {
+  clearInterval(interval)
+  moveDown()
+  indexChange === 1 ? startAlienLeft() : startAlienRight()
+
+}
+
+function moveDown() {
+  console.log('moveDown')
+  for (let i = 0; i < aliensIndex.length; i++) {
+    squares[aliensIndex[i]].classList.remove('alien')
+    aliensIndex[i] = aliensIndex[i] + 14 
+    squares[aliensIndex[i]].classList.add('alien')
+  }
+  if (aliensIndex.some((alien) => alien > 182)) {
+    gameOver()
+  }
+}
+
+function startAlienRight() {
+  console.log('moveRight')
+  interval = setInterval(function () {
+    moveAliens(1)
+  }, intervalSpeed)
+}
+
+function startAlienLeft() {
+  console.log('move left')
+  interval = setInterval(function () {
+    moveAliens(-1)
+  }, intervalSpeed)
+}
+
+function gameOver() {
+  clearInterval(interval)
+  window.alert('GAME OVER')
+}
+
+
+
+
+
+
+
+
+
+
+
 
 let shooterIndex = 188
 
 function drawShooter() {
   squares[shooterIndex].classList.add('shooter')
 }
-
 drawShooter()
 
 // moving elements on page
@@ -59,13 +139,15 @@ function moveShooter(e) {
       break
     case ' ':
       console.log('I pressed the space bar :P')
+      startAlienRight()
+      audioShoot.play()
       squares[shooterIndex - (width - 1)].classList.add('bullet')
   }
 }
 
 document.onkeypress = function (e) {
   e = e || window.event;
-  var charCode = e.keyCode || e.which
+  var charCode = e.keyCode || e.whichf
   if (charCode === 32) {
     e.preventDefault()
     return false
@@ -77,6 +159,7 @@ const bulletTravel = setInterval(() => {
     if (square.classList.contains('bullet') && square.classList.contains('alien')) {
       square.classList.remove('bullet')
       square.classList.remove('alien')
+      aliensIndex.splice(aliensIndex.indexOf(square), 1)
       score += 10
       scoreBoard.textContent = score
     }
@@ -95,8 +178,3 @@ const bulletTravel = setInterval(() => {
 
 document.addEventListener('keydown', moveShooter)
 
-// const moveAliens = setInterval(() => {
-//   aliensMove = document.getElementsByClassName('aliens')[0]
-//   aliensMove.style.top = parseInt(aliensMove.style.top) + 1 + 'px'
-// }
-// }, 600)
